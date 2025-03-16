e09d3866c43a16c18f87cde6c9b4e8b6636661c9
miro <jarbasai@mailfence.com>
2025-03-16 21:04:49 +0000

    youtube support

--- M3U/gen_ai.py
+++ M3U/gen_ai.py
@@ -23,7 +23,7 @@ def remove_emojis(text):
     emoji_pattern = re.compile(
         "[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002702-\U000027B0\U000024C2-\U0001F251]+",
         flags=re.UNICODE)
-    return emoji_pattern.sub("", text).strip()
+    return emoji_pattern.sub("", text).strip().replace(",", "")
 
 
 M3U = ""
